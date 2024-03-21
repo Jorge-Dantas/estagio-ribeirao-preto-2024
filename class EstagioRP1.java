b855986de4e416058f62dcc2d811c08f35cab6a9
Jorge Dantas <101374766+Jorge-Dantas@users.noreply.github.com>
2024-03-21 18:13:35 -0300

    Update class EstagioRP1.java

--- class EstagioRP1.java
+++ class EstagioRP1.java
@@ -1,11 +1,19 @@
-public class EstagioRP1 {
+/*Algoritmo da questão 1) do teste prático =)
+>>> Código 100% feito por Jorge Dantas <<<
+*/
+class EstagioRP1 {
+    // Declarando variáveis e adicionando valores, conforme algoritmo
     public static int INDICE = 13, SOMA = 0, K = 0;
 
     public static void main(String args[]) {
-        while (K < INDICE) {
-            K = K + 1;
-            SOMA = SOMA + K;
+        // Enquanto esta condição não for atendida, o loop executará
+        // (o código dentro do while)
+        while (K < INDICE) { // Enquanto K não for igual ou maior que o valor de INDICE, faça..
+            K = K + 1; // pegue o valor atual da variável K e some 1 ao seu próprio valor
+            SOMA = SOMA + K; // pegue a variável SOMA e some o valor atual de K ao seu próprio valor, fazendo
+                             // um montante..
         }
+        // Mostre na tela o valor final da soma que está na variável SOMA
         System.out.println("Soma igual a: " + SOMA);
     }
 }
